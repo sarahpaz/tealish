98b2a73ad6c927cc0cac572ac4150191b59d2024
Sarah Paz <sarah.paz@gmail.com>
2019-02-18 20:39:11 +0000

    changes to cli class

--- lib/tealish/cli.rb
+++ lib/tealish/cli.rb
@@ -4,31 +4,38 @@ class Tealish::CLI
     puts "     \\      /_]    "
     puts "      \\___ /       "
     puts "Welcome to Tealish!"
-    sleep(1)
-    puts "\nWhich flavor collection would you like to view?"
-    puts "Please select a number from the list below:"
+    flavor_menu
+    select_flavor
+  end
+
+  def flavor_menu
+    puts "Please select a number from the flavor list below:"
     puts <<-DOC
       1. Fruity
       2. Spicy
       3. Floral
       4. Minty
     DOC
-    flavor_option = gets.to_i
-    case flavor_option
-      when 1
-        puts "Fruity teas"
-        url = "https://tealish.com/collections/fruity"
-        Tealish::Scraper.scrape_flavors(url)
-      when 2
-        puts "Spicy teas"
-      when 3
-        puts "Floral teas"
-      when 4
-        puts "Minty teas"
+  end
+
+  def select_flavor
+    input = nil
+    while input != "exit"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "fruity teas"
+      when "2"
+        puts "spicy teas"
+      when "3"
+        puts "floral teas"
+      when "4"
+        puts "minty teas"
       when "exit"
         puts "Thanks for visiting. See you again soon!"
       else
         puts "Sorry, that wasn't a valid option. Please enter a number between 1 - 4."
+      end
     end
   end
 end
