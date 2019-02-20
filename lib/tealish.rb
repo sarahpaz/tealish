3ed1e3c55d398b7fd7e098f0788fd5cdf541f250
Sarah Paz <sarah.paz@gmail.com>
2019-02-20 04:51:12 +0000

    added colorize

--- lib/tealish.rb
+++ lib/tealish.rb
@@ -1,6 +1,7 @@
 require "nokogiri"
 require "open-uri"
 require "pry"
+require "colorize"
 
 require_relative "tealish/version"
 require_relative "tealish/cli"
--- lib/tealish/cli.rb
+++ lib/tealish/cli.rb
@@ -6,38 +6,49 @@ class Tealish::CLI
   end
 
   def welcome
-    puts "     _______ _      "
-    puts "     \\      /_]    "
-    puts "      \\___ /       "
+    puts "     _______ _      ".cyan
+    puts "     \\      /_]    ".cyan
+    puts "      \\___ /       ".cyan
     puts "Welcome to Tealish!"
   end
 
   def menu
     puts "\nHow would you like to shop today?"
-    puts <<~DOC
-      1. Fruity
-      2. Spicy
-      3. Minty
-    DOC
+    puts "1. Fruity".green
+    puts "2. Spicy".red
+    puts "3. Floral".magenta
+
   end
 
   def options
     input = nil
     while input != "exit"
-      puts "\nEnter a number between 1 - 3 to select the category, or enter 'exit'."
       input = gets.strip
       case input
       when "1"
-        puts "Here are our fruity tea options:"
+        puts "*-*-*     Here are our fruity tea options:     *-*-*".green
         url = "https://tealish.com/collections/fruity"
         tea_options = Tealish::Scraper.scrape_teas(url)
-        tea_options.each_with_index do |tea|
-          puts "#{tea.name} - #{tea.type} - #{tea.price}"
+        tea_options.each_with_index do |tea, i|
+          puts "#{i + 1}. #{tea.name} - #{tea.type} - #{tea.price}"
         end
+        puts "\nEnter a number for more details, 'menu' to return to the flavor menu, or 'exit'."
       when "2"
-        puts "Spicy"
+        puts "*-*-*     Here are our spicy tea options:     *-*-*".red
+        url = "https://tealish.com/collections/spicy"
+        tea_options = Tealish::Scraper.scrape_teas(url)
+        tea_options.each_with_index do |tea, i|
+          puts "#{i + 1}. #{tea.name} - #{tea.type} - #{tea.price}"
+        end
+        puts "\nEnter a number for more details, 'menu' to return to the flavor menu, or 'exit'."
       when "3"
-        puts "Minty"
+        puts "*-*-*     Here are our floral tea options:     *-*-*".magenta
+        url = "https://tealish.com/collections/floral"
+        tea_options = Tealish::Scraper.scrape_teas(url)
+        tea_options.each_with_index do |tea, i|
+          puts "#{i + 1}. #{tea.name} - #{tea.type} - #{tea.price}"
+        end
+        puts "\nEnter a number for more details, 'menu' to return to the flavor menu, or 'exit'."
       when "exit"
         puts "Thanks for visiting. See you again soon!"
       when "menu"
@@ -46,5 +57,9 @@ class Tealish::CLI
         puts "Sorry, that option is not valid. Please enter a number between 1 - 3, or type 'exit."
       end
     end
+  end
+
+  def more_details
+    puts ""
   end
 end
