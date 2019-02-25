881e544cbb730113cf46d7661dfa6fe5ad61d8f9
sarahpaz <sarah.paz@gmail.com>
2019-02-25 11:03:21 -0500

    added .find_by_tea

---
+++ lib/tealish/teas.rb
@@ -0,0 +1,18 @@
+class Tealish::Teas
+  attr_accessor :name, :type, :price, :url, :ingredients, :description, :flavor
+  
+    @@all = [] 
+    
+    def initialize(flavor)
+      @flavor = flavor
+      @@all << self
+    end
+
+    def self.all 
+      @@all
+    end
+
+    def self.find_by_flavor(flavor)
+      all.select {|tea| tea.flavor == flavor}
+    end
+end
