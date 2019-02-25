090a87b9326d3eb007442c81dd799849b06c7438
sarahpaz <sarah.paz@gmail.com>
2019-02-25 11:02:40 -0500

    Added flavor class

---
+++ lib/tealish/flavor.rb
@@ -0,0 +1,21 @@
+class Tealish::Flavor
+    attr_accessor :flavor_name
+    attr_reader :teas 
+    
+    @@all = []
+
+    def initialize(flavor_name)
+        @type = type
+        @teas = []
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_tea(tea)
+        self.teas << tea
+        tea.flavor = self
+    end
+end 
